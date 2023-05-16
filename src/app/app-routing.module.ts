ed906b97b9b11edcfd4e7756beba68dc79b75003
sassimed <sassi.med92@gmail.com>
2023-05-16 17:09:07 +0200

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,19 +7,19 @@ const routerOptions: ExtraOptions = {
 };
 
 const routes: Routes = [
-    {
-        path: '', component: AppLayoutComponent,
-        children: [
-            { path: '', data: { breadcrumb: 'Tous les produits' }, loadChildren: () => import('./demo/components/list/listproduct.module').then(m => m.ListProductModule) },
-            { path: 'lights', data: { breadcrumb: 'Lumières' }, loadChildren: () => import('./demo/components/list/listproduct.module').then(m => m.ListProductModule) },
-            { path: 'dashboard', loadChildren: () => import('./demo/components/dashboards/dashboards.module').then(m => m.DashboardsModule) },
-            { path: 'shopping-cart', data: { breadcrumb: 'Panier' }, loadChildren: () => import('./demo/components/shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule) },
-            { path: 'order-history', data: { breadcrumb: 'Historique commandes' }, loadChildren: () => import('./demo/components/orderhistory/orderhistory.module').then(m => m.OrderHistoryModule) },
-            { path: 'checkout-form', data: { breadcrumb: 'Formulaire de paiement' }, loadChildren: () => import('./demo/components/checkoutform/checkoutform.module').then(m => m.CheckoutFormModule) },
-        ]    
-    },
-    { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
-    { path: 'blog', data: { breadcrumb: 'Blog' }, loadChildren: () => import('./demo/components/blog/blog.app.module').then(m => m.BlogAppModule) },
+    // {
+    //     path: '', component: AppLayoutComponent,
+    //     children: [
+    //         { path: '', data: { breadcrumb: 'Tous les produits' }, loadChildren: () => import('./demo/components/list/listproduct.module').then(m => m.ListProductModule) },
+    //         { path: 'lights', data: { breadcrumb: 'Lumières' }, loadChildren: () => import('./demo/components/list/listproduct.module').then(m => m.ListProductModule) },
+    //         { path: 'dashboard', loadChildren: () => import('./demo/components/dashboards/dashboards.module').then(m => m.DashboardsModule) },
+    //         { path: 'shopping-cart', data: { breadcrumb: 'Panier' }, loadChildren: () => import('./demo/components/shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule) },
+    //         { path: 'order-history', data: { breadcrumb: 'Historique commandes' }, loadChildren: () => import('./demo/components/orderhistory/orderhistory.module').then(m => m.OrderHistoryModule) },
+    //         { path: 'checkout-form', data: { breadcrumb: 'Formulaire de paiement' }, loadChildren: () => import('./demo/components/checkoutform/checkoutform.module').then(m => m.CheckoutFormModule) },
+    //     ]    
+    // },
+    { path: '', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
+    // { path: 'blog', data: { breadcrumb: 'Blog' }, loadChildren: () => import('./demo/components/blog/blog.app.module').then(m => m.BlogAppModule) },
     { path: 'notfound', loadChildren: () => import('./demo/components/notfound/notfound.module').then(m => m.NotfoundModule) },
     
 ];
